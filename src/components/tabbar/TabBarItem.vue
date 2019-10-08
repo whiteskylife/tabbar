c02fc91c3e8f73a0e3bab18dbbe067e25f6dda1b
Administrator <981577040@qq.com>
2019-10-09 00:36:25 +0800

    first commit my tabbvar

---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <!--所有的item都展示同一个图片, 同一个文字-->
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    props: {
+      path: String,
+      activeColor: {
+        type: String,
+        default: 'red'
+      }
+    },
+    data() {
+      return {
+        // isActive: true
+      }
+    },
+    computed: {
+      isActive() {
+        // /home -> item1(/home) = true
+        // /home -> item1(/category) = false
+        // /home -> item1(/cart) = true
+        // /home -> item1(/profile) = true
+        return this.$route.path.indexOf(this.path) !== -1             //  $route表示当前活跃的路由
+      },
+      activeStyle() {
+        return this.isActive ? {color: this.activeColor} : {}       // 三目运算符 this.isActive 如果为真，则 activeStyle 值为{color: this.activeColor} ， 否则为空
+      }
+    },
+    methods: {
+      itemClick() {
+        this.$router.replace(this.path)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
